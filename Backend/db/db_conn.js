54025addefe07e7b3ccf8411663e8b9fdb18178f
이수혁 <cg10036@gmail.com>
2020-09-28 09:19:50 +0900

    [BLOOC-9]

--- Backend/db/db_conn.js
+++ Backend/db/db_conn.js
@@ -4,19 +4,26 @@ const config = require("./db_config");
 module.exports = () => {
   return {
     init: () => {
-      return connection;
+      return mysql.createConnection({
+        host: config.host,
+        port: config.port,
+        user: config.user,
+        password: config.password,
+        database: config.database,
+      });
+    },
+    connect: (conn) => {
+      conn.connect((err) => {
+        if (err) throw err;
+      });
     },
   };
 };
 
-let connection = mysql.createConnection({
-  host: config.host,
-  port: config.port,
-  user: config.user,
-  password: config.password,
-  database: config.database,
-});
-
-connection.connect((err) => {
-  if (err) throw err;
-});
+/*
+user
+  id varchar(20)
+  salt varchar(20)
+  iv varchar(100)
+  encBCKey varchar(500)
+*/
