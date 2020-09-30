047693dbd4da24db1b3a31fffaf568e1e52200dd
이수혁 <cg10036@gmail.com>
2020-09-30 15:00:07 +0900

    [BLOOC-8] added ejs render

--- Backend/server.js
+++ Backend/server.js
@@ -2,7 +2,12 @@ const express = require("express");
 const app = express();
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const path = require("path");
 const router = require("./router/router");
+
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.engine("html", require("ejs").renderFile);
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
