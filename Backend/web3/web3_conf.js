870fddbb11d239bb506d2727d0deeeff4e0e1b31
이수혁 <cg10036@gmail.com>
2020-09-30 14:27:10 +0900

    [BLOOC-7] edit web3

--- Backend/web3/web3_conf.js
+++ Backend/web3/web3_conf.js
@@ -1,35 +1,222 @@
 module.exports = {
-  develop_addr: "0x0",
+  develop_addr: "0x5f6aBeCA30Bd69F5996A41828e3A546B1d45ED3A",
   url: "http://localhost:8545/",
-  address: "0x0",
+  cont_address: "0x6f07e3E2DeD28336935977031D98CE5d7Be11316",
   abi: [
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "_owner",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "_approved",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "_tokenId",
+          type: "uint256",
+        },
+      ],
+      name: "Approval",
+      type: "event",
+    },
     {
       constant: false,
       inputs: [
         {
-          name: "_str",
-          type: "string",
+          internalType: "address",
+          name: "_to",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "_tokenId",
+          type: "uint256",
         },
       ],
-      name: "setName",
+      name: "approve",
       outputs: [],
       payable: false,
       stateMutability: "nonpayable",
       type: "function",
     },
     {
-      inputs: [],
+      anonymous: false,
+      inputs: [
+        {
+          indexed: false,
+          internalType: "string",
+          name: "_name",
+          type: "string",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "birth",
+          type: "uint256",
+        },
+      ],
+      name: "CertCreated",
+      type: "event",
+    },
+    {
+      constant: false,
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "_donateDate",
+          type: "uint256",
+        },
+        {
+          internalType: "uint256",
+          name: "_birth",
+          type: "uint256",
+        },
+        {
+          internalType: "uint256",
+          name: "_gender",
+          type: "uint256",
+        },
+        {
+          internalType: "string",
+          name: "_name",
+          type: "string",
+        },
+        {
+          internalType: "string",
+          name: "_kind",
+          type: "string",
+        },
+      ],
+      name: "createCert",
+      outputs: [],
       payable: false,
       stateMutability: "nonpayable",
-      type: "constructor",
+      type: "function",
+    },
+    {
+      constant: false,
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "_tokenId",
+          type: "uint256",
+        },
+      ],
+      name: "takeOwnership",
+      outputs: [],
+      payable: false,
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      constant: false,
+      inputs: [
+        {
+          internalType: "address",
+          name: "_to",
+          type: "address",
+        },
+        {
+          internalType: "uint256",
+          name: "_tokenId",
+          type: "uint256",
+        },
+      ],
+      name: "transfer",
+      outputs: [],
+      payable: false,
+      stateMutability: "nonpayable",
+      type: "function",
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: "address",
+          name: "_from",
+          type: "address",
+        },
+        {
+          indexed: true,
+          internalType: "address",
+          name: "_to",
+          type: "address",
+        },
+        {
+          indexed: false,
+          internalType: "uint256",
+          name: "_tokenId",
+          type: "uint256",
+        },
+      ],
+      name: "Transfer",
+      type: "event",
     },
     {
       constant: true,
-      inputs: [],
-      name: "run",
+      inputs: [
+        {
+          internalType: "address",
+          name: "_owner",
+          type: "address",
+        },
+      ],
+      name: "balanceOf",
       outputs: [
         {
+          internalType: "uint256",
+          name: "_balance",
+          type: "uint256",
+        },
+      ],
+      payable: false,
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      constant: true,
+      inputs: [
+        {
+          internalType: "uint256",
           name: "",
+          type: "uint256",
+        },
+      ],
+      name: "BloodCerts",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "donateDate",
+          type: "uint256",
+        },
+        {
+          internalType: "uint256",
+          name: "birth",
+          type: "uint256",
+        },
+        {
+          internalType: "uint256",
+          name: "gender",
+          type: "uint256",
+        },
+        {
+          internalType: "string",
+          name: "name",
+          type: "string",
+        },
+        {
+          internalType: "string",
+          name: "kind",
           type: "string",
         },
       ],
@@ -39,12 +226,82 @@ module.exports = {
     },
     {
       constant: true,
-      inputs: [],
-      name: "str",
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      name: "certToOwner",
       outputs: [
         {
+          internalType: "address",
           name: "",
-          type: "string",
+          type: "address",
+        },
+      ],
+      payable: false,
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      constant: true,
+      inputs: [
+        {
+          internalType: "address",
+          name: "_owner",
+          type: "address",
+        },
+      ],
+      name: "getCertByOwner",
+      outputs: [
+        {
+          internalType: "uint256[]",
+          name: "",
+          type: "uint256[]",
+        },
+      ],
+      payable: false,
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      constant: true,
+      inputs: [
+        {
+          internalType: "address",
+          name: "",
+          type: "address",
+        },
+      ],
+      name: "ownerCertCount",
+      outputs: [
+        {
+          internalType: "uint256",
+          name: "",
+          type: "uint256",
+        },
+      ],
+      payable: false,
+      stateMutability: "view",
+      type: "function",
+    },
+    {
+      constant: true,
+      inputs: [
+        {
+          internalType: "uint256",
+          name: "_tokenId",
+          type: "uint256",
+        },
+      ],
+      name: "ownerOf",
+      outputs: [
+        {
+          internalType: "address",
+          name: "_owner",
+          type: "address",
         },
       ],
       payable: false,
