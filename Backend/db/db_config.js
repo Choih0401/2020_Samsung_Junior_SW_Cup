3d8396882e6c07a3863e38d0e0afd28cd6301c68
이수혁 <cg10036@gmail.com>
2020-10-17 20:50:13 +0900

    [BLOOC-5] change conf

--- Backend/db/db_config.js
+++ Backend/db/db_config.js
@@ -1,7 +1,7 @@
 module.exports = {
   host: "localhost",
   port: 3306,
-  user: "root",
-  password: "ch04010508",
-  database: "test",
+  user: "samsung",
+  password: "th15_15_s4f3_p4ssw0rd!",
+  database: "samsung",
 };
