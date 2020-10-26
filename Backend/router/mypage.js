c0e0f43287ebc1e3d7469be68f9e0d20ddb26466
bomin5957 <bomin595758@naver.com>
2020-10-26 22:30:13 +0900

    mypage edit

--- Backend/router/mypage.js
+++ Backend/router/mypage.js
@@ -22,7 +22,7 @@ getBloodCerts = async (address, result) => {
 };
 
 router.get("/", (req, res) => {
-    res.render("mypage", {bloodCerts: async(address, result)});
+    res.render("mypage");
 })
 
 module.exports = router;
