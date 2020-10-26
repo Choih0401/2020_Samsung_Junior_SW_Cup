634e3b9a295dfd5af348da3b3ad2a22f4daeb6ed
이수혁 <cg10036@gmail.com>
2020-10-27 01:25:00 +0900

    give

--- Backend/router/give.js
+++ Backend/router/give.js
@@ -22,48 +22,51 @@ getBloodCerts = async (address, result) => {
 
 // num, to
 router.post("/", (req, res) => {
-  if(isNaN(req.body.num) || req.body.num <= 0 || !req.body.to) {
-      return res.status(400).json();
-  }
-  web3
+    if(isNaN(req.body.num) || req.body.num <= 0 || !req.body.to) {
+        return res.status(400).json();
+    }
+    web3
     .getCertByOwner(req.session.address, req.session.address)
     .then((result) => {
-      console.log(result)
-      if(result.length < req.body.num) {
-          return res.send("<script>alert('초과')</script>");
-      }
-      conn.query("SELECT * FROM user WHERE id=?", [req.body.to], (err, rows, fields) => {
-          if(rows.length == 0) {
-              return res.send("<script>alert('없는아이디')<script>");
-          }
-          web3.unlockAccount(req.session.address, req.session.BCKey)
-            .then(() => {
-            (async () => {
-                for(let i = 0;i < req.body.num;i++) {
-                    await web3.transfer(req.session.address, rows[0].address, result[i]);
+        console.log(result)
+         getBloodCerts(req.session.address, result)
+        .then((bloodCerts) => {
+            let a = []
+            let b = []
+            for(let i = 0;i < bloodCerts.length;i++) {
+                console.log(bloodCerts[i]);
+                if(bloodCerts[i].used == 0) {
+                    a.push(result[i]);
+                    b.push(bloodCerts[i])
+                    if(a.length >= req.body.num) {
+                        break;
+                    }
                 }
-                return res.send("<script>alert('success')</script>")
-            })();
-        });
-      })
-    //   getBloodCerts(req.session.address, result)
-    //     .then((bloodCerts) => {
-    //       conn.query("SELECT num, title, name FROM board ORDER BY num DESC", [], (err, rows, fields) => {
-    //         if(err) return res.status(500).json();
-    //         bloodCerts = bloodCerts || [];
-    //         console.log("헌혈증: ", bloodCerts);
-    //         console.log("이름: " + req.session.name);
-    //         console.log("개수: " + bloodCerts.length);
-    //         res.render("index", {
-    //           bloodCerts: bloodCerts,
-    //           name: req.session.name,
-    //           page: req.params.num,
-    //           board: rows
-    //         });
-    //       })
-    //     })
-    //     .catch(console.error);
-    });
+            }
+            if(a.length < req.body.num) {
+                return res.send("<script>alert('초과')</script>");
+            }
+            conn.query("SELECT * FROM user WHERE id=?", [req.body.to], (err, rows, fields) => {
+                if(rows.length == 0) {
+                    return res.send("<script>alert('없는아이디')<script>");
+                }
+                web3.unlockAccount(req.session.address, req.session.BCKey)
+                .then(() => {
+                    web3.unlockAccount(rows[0].address, rows[0].BCKey)
+                    .then(() => {
+                        (async () => {
+                            for(let i = 0;i < req.body.num;i++) {
+                                await web3.createCert(rows[0].address, b[i].donateDate, b[i].birth, b[i].gender, b[i].name, b[i].kind)
+                                await web3.use(req.session.address, a[i])
+                            }
+                            return res.send("<script>alert('success')</script>")
+                        })();
+                    })
+                })
+            });
+        })
+        .catch(console.error);
+    })
 })
 
 module.exports = router;
